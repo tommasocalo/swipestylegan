909943e05d6146b4d1da7dc32037d077acb99583
tommasocalo <tommycalo96@gmail.com>
2024-07-04 18:57:54 -0400

    first commit

---
+++ FaceNet_embeddings.py
@@ -0,0 +1,24 @@
+# calculate a face embedding for each face in the dataset using facenet
+from numpy import load
+from numpy import expand_dims
+from numpy import asarray
+from numpy import savez_compressed
+from keras.models import load_model
+
+# get the face embedding for one face
+def get_embedding(model, face_pixels):
+	# scale pixel values
+	face_pixels = face_pixels.astype('float32')
+	# standardize pixel values across channels (global)
+	mean, std = face_pixels.mean(), face_pixels.std()
+	face_pixels = (face_pixels - mean) / std
+	# transform face into one sample
+	samples = expand_dims(face_pixels, axis=0)
+	# make prediction to get embedding
+	yhat = model.predict(samples)
+	return yhat[0]
+
+
+# load the facenet model
+model = load_model('/home/tcalo3/projects/project6/src/modules/metrics/models/keras-facenet/model/facenet_keras.h5')
+print('Model Loaded')
